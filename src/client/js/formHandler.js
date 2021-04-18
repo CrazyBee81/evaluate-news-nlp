ef5175484ce288e0b934b98cee0cf3fab41b1ac2
Beny <beny@gujnm271116.global.intra.guj.com>
2021-04-18 11:31:13 +0200

    stringify newData

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -10,28 +10,31 @@ function handleSubmit(event) {
 
     function performAction(e) {
         const formdata = new FormData();
-        formdata.append("key", getAPIKey());
-        formdata.append("url", document.querySelector('#url').value);
-        formdata.append("lang", document.querySelector('#lang').value);
-
-        if (Client.validateImput(formdata)) {
-            const requestOptions = {
-                method: 'POST',
-                body: formdata,
-                redirect: 'follow'
-            };
-            getNLPData(baseURL, requestOptions).then((response) => {
-                console.log(response)
-                document.getElementById('model').innerHTML = response.model;
-                document.getElementById('agreement').innerHTML = response.agreement;
-                document.getElementById('irony').innerHTML = response.irony;
-                document.getElementById('polarity').innerHTML = response.score_tag;
-            })
-        }
+        getData("http://localhost:8082/key").then((response) => {
+            formdata.append("key", response);
+            formdata.append("url", document.querySelector('#url').value);
+            formdata.append("lang", document.querySelector('#lang').value);
+            if (Client.validateImput(formdata)) {
+                postData(baseURL, formdata).then((response) => {
+                    let newData = response;
+                    newData = {
+                        model: newData.model,
+                        agreement: newData.agreement,
+                        irony: newData.irony,
+                        score_tag: newData.score_tag
+                    }
+                    console.log(newData);
+                    postData('/add', {}, JSON.stringify({newData}).then(() => {
+                        updateUI()
+                    });
+                })
+            }
+        })
     }
 
-    const getAPIKey = async () => {
-        const response = await fetch('http://localhost:8082/key');
+    const getData = async (url) => {
+        console.log(url);
+        const response = await fetch(url)
         try {
             const newData = await response.json();
             return newData;
@@ -40,8 +43,9 @@ function handleSubmit(event) {
         }
     }
 
-    const getNLPData = async (baseURL, requestOptions) => {
-        const response = await fetch(baseURL, requestOptions)
+    // Async POST
+    const postData = async (url = '', data = {}) => {
+        const response = await fetch(url, {method: 'POST', body: data});
         try {
             const newData = await response.json();
             console.log(newData)
@@ -49,19 +53,18 @@ function handleSubmit(event) {
         } catch (error) {
             console.log("error", error);
         }
-    }
-
+    };
 }
 
 // updateUI
 const updateUI = async () => {
-
     try {
-        const newData = await response.data;
-        console.log(newData);
-        document.getElementById('temp').innerHTML = Math.round(newData.temp) + '°C';
-        document.getElementById('date').innerHTML = newData.date;
-        document.getElementById('content').innerHTML = newData.feelings;
+        const response = await fetch("http://localhost:8082/all");
+        console.log(response);
+        document.getElementById('model').innerHTML = response.model;
+        document.getElementById('agreement').innerHTML = response.agreement;
+        document.getElementById('irony').innerHTML = response.irony;
+        document.getElementById('polarity').innerHTML = response.score_tag;
     } catch (error) {
         console.log("error", error);
     }
--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+// Setup empty JS object to act as endpoint for all routes
+const projectData = {};
+
 var path = require('path')
 const express = require('express')
 var bodyParser = require('body-parser')
@@ -30,3 +33,24 @@ app.get('/key', function (req, res) {
     res.json(process.env.API_KEY)
 })
 
+// POST route
+app.post('/add', addMeaningfulData);
+
+function addMeaningfulData(req,res){
+
+    let newEntry = {
+        agreement: req.body.agreement,
+        score_tag: req.body.score_tag,
+        irony: req.body.irony,
+        model: req.body.model
+    }
+
+    projectData[Object.keys(projectData).length] = newEntry;
+    console.log(newEntry)
+    res.send(projectData[0])
+}
+
+// GET route
+app.get('/all', function (req, res) {
+    res.send(projectData[Object.keys(projectData).length-1])
+})
